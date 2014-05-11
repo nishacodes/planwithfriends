ac0c76a0ca40c18af2e78f78d49ce0fb894f05f4
Nisha Batra <nishabatra27@gmail.com>
2014-05-11 17:55:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planwithfriends::Application.config.secret_token = 'b66c39da296492a74129645001f577c56740354ca3618381c96954abc8a5f01f8dcc575fcdcecd6482a1a61407e52c19a2414bf8eb0fc7093ddf1f0d782aec7f'
